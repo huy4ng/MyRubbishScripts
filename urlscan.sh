39cc74cc9ca1372bd6566bc60df0232762ac2ea9
huy4ng <52245050+huy4ng@users.noreply.github.com>
2021-06-08 17:01:03 +0800

    Update urlscan.sh

--- urlscan.sh
+++ urlscan.sh
@@ -1,2 +1,2 @@
-＃！/bin/bash
-curl -s " https://urlscan.io/api/v1/search/?q=domain: $1 "  | grep -E ' “网址” '  | cut -d ' " ' -f4 | grep -F $1  | sort -u
+#!/bin/bash
+curl -s "https://urlscan.io/api/v1/search/?q=domain:$1" | grep -E '"url"' | cut -d '"' -f4 | grep -F $1 | sort -u
