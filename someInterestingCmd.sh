9875cb67192e2cb3de52505090ca7ed3fef4a805
huy4ng <marvel@zeros-hu.com>
2021-06-18 16:36:52 +0800

    use nuclei to get info

--- someInterestingCmd.sh
+++ someInterestingCmd.sh
@@ -6,3 +6,6 @@ subfinder -d example.com -silent | waybackurls >> vul3.txt
 cat vul1.txt vuln2.txt vul3.txt | grep “=” | sort -u | grep “?” | httpx -silent >> FUZZvul.txt
 # ssrf fuzz 使用httpx指定burp代理
 xargs -a /root/magicparameter/ssrf.txt -I@ bash -c ‘for url in $(cat FUZZvul.txt); do echo “$url&@=http://burpcollabrator.net”;done’ | httpx -http-proxy http://127.0.0.1:8080
+# 使用nuclei提取页面token
+cat urls.txt | assestfinder | httprobe | gauplus | nuclei -t exposures/tokens/
+nuclei -u URL -t headless/extract-url.yaml -headless -silent | grep "^http" | nuclei -t exposures/tokens/
