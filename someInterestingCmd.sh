3f4463f334d67bef012390623f1394806498465b
huy4ng <52245050+huy4ng@users.noreply.github.com>
2022-08-03 11:30:50 +0800

    Update someInterestingCmd.sh

--- someInterestingCmd.sh
+++ someInterestingCmd.sh
@@ -33,3 +33,24 @@ nmap -sS -T4 -A -p- -iL live_subdomains.txt --min-rate 1000 --max-retries 3
 
 # use subfinder anew naabu httpx nuclei
 while true; do subfinder -dL domains.txt -all | anew subs.txt | naabu -silent -p - | httpx -silent | nuclei -t nuclei-templates/ | notify ; sleep 3600; done
+#If you collected a bunch of alive IP addresses. Firstly make a Full port scanning then run it
+rustscan -a 'hosts.txt' -r 1-65535 | grep Open | tee open_ports.txt | sed 's/Open //' | httpx -silent | nuclei -t ~/nuclei-templates/
+
+#Fingerprinting with Shodan and Nuclei engine:
+shodan domain example.com | awk '{print $3}' | httpx -silent | nuclei -t /home/ofjaaah/PENTESTER/nuclei-templates/
+
+#Fully automate your bug bounty with Nuclei and a sprinkle of bash
+#/bin/bash
+curl -sL https://github.com/projectdiscovery/public-bugbounty-programs/raw/master/chaos-bugbounty-list.json | jq -r '-programs[] |select(.bounty == true) | .domains' | sort -u | cut -d '"' -f2 > urls.txt;
+cat urls.txt | sed 's/[]//g' | sed 's/[//g' | sed 's/]//g' | sed -r '/*\s*$/d' > prepared.txt;
+for n in $(cat prepared.txt); do
+echo -e “Starting main scanner"; echo -e "Scanning: $n";
+chaos -bbq -silent -d $n | anew | httprobe -c 5000 | nuclei -t $HOME/nuclei-templates -severity critical,high,medium -retries 1 -timeout 3 -c 3000 -rate-limit 3000 -o nuclei/$n.txt -project -project-path nuclei/project -silent;
+done
+echo -e "Cleaning up empty scans and other debris"
+rm urls.txt && rm prepared.txt
+find nuclei/. -size @ -delete
+echo -e "Scan completed."
+
+#Boom p1 in one command
+recon |xargs| grep -ax( “$sub.example.com) &> /dev/null > outtmp1.txt | httprobe -c 10000000000000 > gau | screenshots  -/outdir1/as> xssscnner300 < "vulntest.txt" | nuclei -T $TARGET | masscan --rate 9999999999 | sort -uqa >/tmp/ > all_final_final.html
